0fd6f3714f3bbeadb0074b8514b775c8d04e0f8b
andy30696 <andy30696@hotmail.com>
2023-08-25 00:32:34 -0300

    Footter and slider

--- js/index.js
+++ js/index.js
@@ -1,35 +1,23 @@
-const slides = document.querySelector(".slides");
-const slideImages = document.querySelectorAll(".slides img");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
-let counter = 1;
-const size = slideImages[0].clientWidth;
+document.addEventListener('DOMContentLoaded', function() {
+  const slides = document.querySelector(".slides");
+  const slideWidth = document.querySelector(".slidesImg").offsetWidth;
+  const prevBtn = document.querySelector(".prev");
+  const nextBtn = document.querySelector(".next");
+  let currentPosition = 0;
 
-slides.style.transform = `translateX(${-size * counter}px)`;
+  prevBtn.addEventListener("click", () => {
+    currentPosition += slideWidth;
+    if (currentPosition > 0) {
+      currentPosition = -(slideWidth * 4);
+    }
+    slides.style.transform = `translateX(${currentPosition}px)`;
+  });
 
-nextBtn.addEventListener("click", () => {
-  if (counter >= slideImages.length - 1) return;
-  slides.style.transition = "transform 0.6s ease";
-  counter++;
-  slides.style.transform = `translateX(${-size * counter}px)`;
-});
-
-prevBtn.addEventListener("click", () => {
-  if (counter <= 0) return;
-  slides.style.transition = "transform 0.6s ease";
-  counter--;
-  slides.style.transform = `translateX(${-size * counter}px)`;
-});
-
-slides.addEventListener("transitionend", () => {
-  if (slideImages[counter].id === "lastClone") {
-    slides.style.transition = "none";
-    counter = slideImages.length - 2;
-    slides.style.transform = `translateX(${-size * counter}px)`;
-  }
-  if (slideImages[counter].id === "firstClone") {
-    slides.style.transition = "none";
-    counter = slideImages.length - counter;
-    slides.style.transform = `translateX(${-size * counter}px)`;
-  }
+  nextBtn.addEventListener("click", () => {
+    currentPosition -= slideWidth;
+    if (currentPosition < -(slideWidth * 4)) {
+      currentPosition = 0;
+    }
+    slides.style.transform = `translateX(${currentPosition}px)`;
+  });
 });
